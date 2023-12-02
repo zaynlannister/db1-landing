a1ed48add3801cf9570fed5dccdff5f70ad41099
zaynlannister <zaynlannister@gmail.com>
2023-12-02 18:26:47 +0500

    feat: add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ContactSection from "./components/Contact/ContactSection";
 import FeatureSection from "./components/Feature/FeatureSection";
 import HeaderSection from "./components/Header/HeaderSection";
 import ServicesSection from "./components/Services/ServicesSection";
@@ -13,6 +14,9 @@ const App = () => {
       </>
       <>
         <ServicesSection />
+      </>
+      <>
+        <ContactSection />
       </>
     </div>
   );
---
+++ src/components/Contact/ContactSection.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const ContactSection = () => {
+  return (
+    <StyledContactSection className="contact-container py-20 mt-16">
+      <div className="my-container text-[#fff]">
+        <div className="text-center">
+          <p className="text-[45px] font-bold contact-title">
+            Want to get our offer updates and news?
+          </p>
+          <p>
+            Submit your e-mail and we will give you update about information and
+            discount. Every single week. (not spamming)
+          </p>
+        </div>
+        <div className="flex justify-center contact-action mt-8">
+          <input
+            className="px-4 w-[350px] text-black"
+            type="text"
+            placeholder="Enter your e-mail here"
+          />
+          <button className="primary-button">Submit</button>
+        </div>
+      </div>
+    </StyledContactSection>
+  );
+};
+
+const StyledContactSection = styled.div`
+  background-image: url("/img/contact-bg.jpg");
+  background-size: cover;
+
+  .contact-action {
+    input {
+      outline: none;
+    }
+  }
+
+  @media (max-width: 800px) {
+    .contact-title {
+      font-size: 26px;
+      margin-bottom: 10px;
+    }
+  }
+
+  @media (max-width: 350px) {
+    .contact-action input {
+      width: 100%;
+    }
+  }
+`;
+
+export default ContactSection;
